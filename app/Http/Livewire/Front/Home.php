255425733601e45e840e4081590fe5514d69b3db
samankassou <samankassoufoulla@gmail.com>
2021-10-31 07:39:30 +0100

    routes refactoring

--- app/Http/Livewire/Front/Home.php
+++ app/Http/Livewire/Front/Home.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Livewire\Front_office;
+namespace App\Http\Livewire\Front;
 
 use App\Models\Advert;
 use Livewire\Component;
@@ -9,9 +9,10 @@ class Home extends Component
 {
     public function render()
     {
+        $this->emit("success", __("Success:"), __("Courier updated!"));
         $latestAds = Advert::latest()->paginate(10);
 
-        return view('livewire.front_office.home', [
+        return view('livewire.front.home', [
             'adverts' => $latestAds
         ])
         ->extends('layouts.app')
