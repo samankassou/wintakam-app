08d079cb9a273fdd0f40c9222c39e1b3f576f901
samankassou <samankassoufoulla@gmail.com>
2021-11-06 21:12:39 +0100

    wip

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -22,17 +22,22 @@
         <div class="px-4 py-8 bg-white shadow sm:px-10">
             <form wire:submit.prevent="register">
                 <div>
-                    <div class="relative mb-4 flex items-center flex-col">
-                        <div class="relative overflow-hidden w-20 h-20 rounded-full">
+                    <div x-data="{ isUploading: false, progress: 0}"
+                x-on:livewire-upload-start="isUploading = true"
+                x-on:livewire-upload-finish="isUploading = false"
+                x-on:livewire-upload-error="isUploading = false"
+                x-on:livewire-upload-progress="progress = $event.detail.progress">
+                        <div class="relative mb-4 flex items-center flex-col">
+                            <div class="relative overflow-hidden w-20 h-20 rounded-full">
+                                @if ($picture)
+                                <img class="absolute inset-0 h-full w-full object-cover object-center" src="{{ $picture->temporaryUrl() }}"
+                                    alt="Avatar Upload" />
+                                @else
+                                <img class="absolute inset-0 h-full w-full object-cover object-center"
+                                    src="{{ asset('front_office/images/avatar-placeholder.png') }}" alt="Avatar Upload" />
+                                @endif
+                            </div>
                             @if ($picture)
-                            <img class="absolute inset-0 h-full w-full object-cover object-center" src="{{ $picture->temporaryUrl() }}"
-                                alt="Avatar Upload" />
-                            @else
-                            <img class="absolute inset-0 h-full w-full object-cover object-center"
-                                src="{{ asset('front_office/images/avatar-placeholder.png') }}" alt="Avatar Upload" />
-                            @endif
-                        </div>
-                        @if ($picture)
                             <div class="absolute w-20 h-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full">
                                 <svg wire:click="deletePicture" xmlns="http://www.w3.org/2000/svg"
                                     class="h-8 w-8 p-2 bg-green-500 rounded-full text-gray-100 cursor-pointer absolute bottom-0 right-0 hover:text-red-500 transition-colors"
@@ -41,22 +46,26 @@
                                         d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                 </svg>
                             </div>
-                        @endif
+                            @endif
 
-                    </div>
-                    <div class="text-center my-2">
-                        @if (!$picture)
-                        <label class="cursor-pointer mt-6">
-                            <span wire:loading.attr="disabled" wire:target="picture"
-                                class="mt-2 leading-normal px-4 py-2 bg-green-500 hover:bg-green-300 text-white mx-auto text-sm disabled:opacity-50">Choisir
-                                une photo de profil</span>
-                            <input type='file' class="hidden" name="picture" wire:model="picture" />
-                        </label>
-                        @endif
-                    </div>
-                    @error('picture')
+                        </div>
+                        <div class="text-center my-2">
+                            @if (!$picture)
+                            <label class="cursor-pointer mt-6">
+                                <span wire:loading.attr="disabled" wire:target="picture"
+                                    class="mt-2 leading-normal px-4 py-2 bg-green-500 hover:bg-green-300 text-white mx-auto text-sm disabled:opacity-50">Choisir
+                                    une photo de profil</span>
+                                <input type='file' class="hidden" name="picture" wire:model="picture" />
+                            </label>
+                            @endif
+                        </div>
+                        <div x-show="isUploading">
+                            <progress max="100" x-bind:value="progress" x-text="progress"></progress>
+                        </div>
+                        @error('picture')
                         <p class="mt-2 text-sm text-red-600 text-center">{{ $message }}</p>
-                    @enderror
+                        @enderror
+                    </div>
 
                     <label for="name" class="block text-sm font-medium text-gray-700 leading-5">
                         Nom
